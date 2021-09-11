d929a54bef1180da6e0575f066cb9df09c6dde97
rnm195 <90164842+rnm195@users.noreply.github.com>
2021-09-11 11:30:50 +1000

    Update parse-config.js

--- parse-config.js
+++ parse-config.js
@@ -2,8 +2,8 @@ const PORT =  process.env.PORT || 1337;
 
 module.exports = exports = {
   APP_NAME: 'Heroku Parse',
-  APP_ID: 'an-example-app-id',
-  MASTER_KEY: 'a-super-secret-master-key',
+  APP_ID: 'MSP Test',
+  MASTER_KEY: 'a-super-secret-master-key1',
   PORT: PORT,
   SERVER_URL: process.env.SERVER_URL || `http://localhost:${PORT}`,
   DATABASE_URL: process.env.DATABASE_URL || 'postgres://parse:parse@localhost:5432/parse',
